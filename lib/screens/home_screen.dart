9a00a546622f625f7ce48ab0bc005141df7ccacc
rentax <123456>
2020-02-05 19:32:44 +0530

    Rows and Columns

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,21 +7,84 @@ class HomeScreen extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Center(
         child: Container(
-          alignment: Alignment.center,
-          color: Colors.purple,
+          padding: EdgeInsets.all(15.0),
+            alignment: Alignment.center,
+            color: Colors.purple,
 //          width: 200.0,
 //          height: 100.0,
 //          margin: EdgeInsets.all(20.0),
-          child: Text(
-            "Swati",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-                decoration: TextDecoration.none,
-                fontSize: 75.0,
-                fontFamily: 'Poppins',
-                color: Colors.white),
-          ),
-        ),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Swati",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30.0,
+                          fontFamily: 'Poppins',
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Sanjay",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30.0,
+                          fontFamily: 'Poppins',
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Solanki",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30.0,
+                          fontFamily: 'Poppins',
+                          color: Colors.white),
+                    ))
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Text(
+                      "Swati",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30.0,
+                          fontFamily: 'Poppins',
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Sanjay",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30.0,
+                          fontFamily: 'Poppins',
+                          color: Colors.white),
+                    )),
+                    Expanded(
+                        child: Text(
+                      "Solanki",
+                      textDirection: TextDirection.ltr,
+                      style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontSize: 30.0,
+                          fontFamily: 'Poppins',
+                          color: Colors.white),
+                    ))
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
